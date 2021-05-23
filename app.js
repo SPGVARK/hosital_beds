88f259e448d91c48cdce7e9810f2775b2295bab2
SPGVARK <83496037+SPGVARK@users.noreply.github.com>
2021-05-23 10:00:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
     const express = require('express');
     const app = express();
+    const cors = require('cors');
     const test2 = require('./BedDataCollector')
     app.use('/api/*',(req,res,next)=>{
         console.log("tested");
         next();
     })
-    app.get('/api/beds',(req,res)=>
+    app.get('/api/beds',cors(),(req,res)=>
     {
         res.json(require('./data.json'));
     })
