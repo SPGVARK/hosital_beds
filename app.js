f8ec46c882bab4ce481589d7463c63b3d54766a9
SPGVARK <83496037+SPGVARK@users.noreply.github.com>
2021-05-23 10:06:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
     const express = require('express');
     const port = process.env.PORT||5000;
     const app = express();
+    const cors = require('cors');
     const test2 = require('./BedDataCollector')
     app.use('/api/*',(req,res,next)=>{
-        console.log("tested");
+        //console.log("tested");
         next();
     })
     app.get('/',(req,res)=>{
-        res.send("Hello World");
+        res.send("SPGVARK Cannot GET /");
     })
-    app.get('/api/beds',(req,res)=>
+    app.get('/api/beds',cors(),(req,res)=>
     {
         res.set('Cache-control', 'public, max-age=2500')
         res.json(require('./data.json'));
